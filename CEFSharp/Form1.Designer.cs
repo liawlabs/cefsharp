e854982d0dd4dc81762498e539d2eb8cfbf8ebdf
Sze Wong Liaw <liawsw@live.com>
2023-04-26 21:10:30 +0800

    Add project files.

---
+++ CEFSharp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CEFSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browser = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dtp = new System.Windows.Forms.DateTimePicker();
+            this.cbApp = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // browser
+            // 
+            this.browser.ActivateBrowserOnCreation = false;
+            this.browser.Dock = System.Windows.Forms.DockStyle.Left;
+            this.browser.Location = new System.Drawing.Point(0, 0);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(391, 316);
+            this.browser.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(413, 41);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // dtp
+            // 
+            this.dtp.Location = new System.Drawing.Point(413, 115);
+            this.dtp.Name = "dtp";
+            this.dtp.Size = new System.Drawing.Size(200, 20);
+            this.dtp.TabIndex = 2;
+            this.dtp.ValueChanged += new System.EventHandler(this.dtp_ValueChanged);
+            // 
+            // cbApp
+            // 
+            this.cbApp.FormattingEnabled = true;
+            this.cbApp.Items.AddRange(new object[] {
+            "WinForm",
+            "WPF",
+            "UWP"});
+            this.cbApp.Location = new System.Drawing.Point(413, 78);
+            this.cbApp.Name = "cbApp";
+            this.cbApp.Size = new System.Drawing.Size(121, 21);
+            this.cbApp.TabIndex = 3;
+            this.cbApp.SelectedIndexChanged += new System.EventHandler(this.cbApp_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 316);
+            this.Controls.Add(this.cbApp);
+            this.Controls.Add(this.dtp);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.browser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser browser;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DateTimePicker dtp;
+        private System.Windows.Forms.ComboBox cbApp;
+    }
+}
+
---
+++ CEFSharp/Form1.cs
@@ -0,0 +1,127 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+//https://ourcodeworld.com/articles/read/173/how-to-use-cefsharp-chromium-embedded-framework-csharp-in-a-winforms-application#:~:text=To%20add%20CefSharp%2C%20go%20to,WinForms%20distributtion%20and%20install%20it.&text=Follow%20the%20installation%20setup%20(accept%20therms%20and%20install).
+//https://www.youtube.com/watch?v=fOzBVy-sDbM
+//https://github.com/cefsharp/CefSharp
+//https://getbootstrap.com/docs/5.3/getting-started/introduction/
+//https://grantwinney.com/hosting-a-simple-webpage-in-winforms-with-cefsharp/
+
+namespace CEFSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            browser.KeyboardHandler = new CustomKeyboardHandler(txtName, dtp);
+
+            browser.LoadingStateChanged += Browser_LoadingStateChanged;
+            browser.JavascriptMessageReceived += Browser_JavascriptMessageReceived;
+
+
+            browser.LoadUrl($@"{Application.StartupPath}\index.html");
+        }
+
+        private void Browser_JavascriptMessageReceived(object sender, JavascriptMessageReceivedEventArgs e)
+        {
+            var message = ((IDictionary<string, object>)e.Message).Single();
+            switch (message.Key)
+            {
+                case "appr":
+                    dtp.Invoke(new Action(() =>
+                        dtp.Value = DateTime.TryParse(message.Value.ToString(), out var apprvDate) ? apprvDate : DateTime.Now
+                    ));
+                    break;
+                case "app":
+                    cbApp.Invoke(new Action(() =>
+                    {
+                        if (message.Value.ToString() != "")
+                            cbApp.Text = message.Value.ToString();
+                        else
+                            cbApp.SelectedIndex = -1;
+                    }));
+                    break;
+            }
+        }
+
+        private void Browser_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            if (!e.IsLoading)
+            {
+                browser.ExecuteScriptAsync(@"
+                let app = document.getElementById('application');
+                app.addEventListener('change', (e) => {
+                    CefSharp.PostMessage({app: application.options[application.selectedIndex].text});
+                });
+
+                let apprv = document.getElementById('approval');
+                apprv.addEventListener('input', (e) => {
+                    CefSharp.PostMessage({apprv: approval.value});
+                });
+            ");
+            }
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            browser.ExecuteScriptAsync($"document.getElementById('name').value = '{txtName.Text}';");
+        }
+
+        private void dtp_ValueChanged(object sender, EventArgs e)
+        {
+            browser.ExecuteScriptAsync($"document.getElementById('approval').value = '{dtp.Value:yyyy-MM-ddThh:mm}';");
+        }
+
+        private void cbApp_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            browser.ExecuteScriptAsync($"document.getElementById('application').value = '{cbApp.Text}';");
+        }
+    }
+}
+
+public class CustomKeyboardHandler : IKeyboardHandler
+{
+    private readonly TextBox txtName;
+    private readonly DateTimePicker dtp;
+
+    public CustomKeyboardHandler(TextBox txtName, DateTimePicker dtp)
+    {
+        this.txtName = txtName;
+        this.dtp = dtp;
+    }
+
+    public bool OnKeyEvent(IWebBrowser chromiumWebBrowser, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey)
+    {
+        chromiumWebBrowser.EvaluateScriptAsync("document.getElementById('name').value;")
+                            .ContinueWith(x => txtName.Invoke(new Action(() =>
+                            {
+                                txtName.Text = Convert.ToString(x.Result.Result);
+                            })));
+
+        chromiumWebBrowser.EvaluateScriptAsync("document.getElementById('approval').value;")
+                            .ContinueWith(x => dtp.Invoke(new Action(() =>
+                            {
+                                dtp.Value = DateTime.TryParse(x.Result.Result.ToString(), out var aprvDate) ? aprvDate : DateTime.Now;
+                            })));
+
+        return true;
+    }
+
+    public bool OnPreKeyEvent(IWebBrowser chromiumWebBrowser, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey, ref bool isKeyboardShortcut)
+    {
+        // If you return true here, the keyboard event is considered 'handled', and OnKeyEvent will not fire.
+        return false;
+    }
+}
